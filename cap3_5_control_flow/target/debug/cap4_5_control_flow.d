9becbc9911d7a86d8869ac8b6f77b89f6b5fd2dc
stevenn-dev8 <stevenndev8@gmail.com>
2025-04-15 22:30:14 -0300

    cap3 conditional

---
+++ cap3_5_control_flow/target/debug/cap4_5_control_flow.d
@@ -0,0 +1,1 @@
+/home/steven/Steven/learn-rust/cap3_5_control_flow/target/debug/cap4_5_control_flow: /home/steven/Steven/learn-rust/cap3_5_control_flow/src/main.rs
---
+++ cap3_5_control_flow/target/debug/deps/cap4_5_control_flow-ba418f856dd4bc05.d
@@ -0,0 +1,5 @@
+/home/steven/Steven/learn-rust/cap3_5_control_flow/target/debug/deps/cap4_5_control_flow-ba418f856dd4bc05: src/main.rs
+
+/home/steven/Steven/learn-rust/cap3_5_control_flow/target/debug/deps/cap4_5_control_flow-ba418f856dd4bc05.d: src/main.rs
+
+src/main.rs:
