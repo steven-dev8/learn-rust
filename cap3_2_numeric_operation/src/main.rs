cb88ad75d60c5266a941d452f7cdb8f6f96ad16a
stevenn-dev8 <stevenndev8@gmail.com>
2025-04-15 21:14:08 -0300

    cap3 numeric operation

---
+++ cap3_2_numeric_operation/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // adition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    //division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3;
+
+    // remainder
+    let remainder = 45 % 4;
+
+    println!("{remainder}");
+}
