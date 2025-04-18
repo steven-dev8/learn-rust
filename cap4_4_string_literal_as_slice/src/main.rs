49a0f947f1140be017c7960f6544bd33dc0dbc32
stevenn-dev8 <stevenndev8@gmail.com>
2025-04-18 15:05:02 -0300

    cap4 string literal as slice

--- cap4_4_string_literal_as_slice/src/main.rs
+++ cap4_4_string_literal_as_slice/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    
+    let s1 = "Steven Araújo";
+    let s2 = String::from("Livia Araújo da Costa");
+
+    let word = first_word(&s1); // Literal string
+    println!("{word}");
+
+    let word = first_word(&s2); // String
+    println!("{word}");
+
+    let word = first_word(&s2[6..]); // Slice string
+    println!("{word}")
+}
+
+// First word aceita tanto uma string literal, String ou uma slice string
+fn first_word(s: &str) -> &str { 
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i]
+        }
+    }
+    &s[..]
 }
