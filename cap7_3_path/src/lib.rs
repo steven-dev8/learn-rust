91498853cfac954a8415d890e81ca35dfe82ed01
stevenn-dev8 <stevenndev8@gmail.com>
2025-04-21 17:42:54 -0300

    package path

---
+++ cap7_3_path/src/lib.rs
@@ -0,0 +1,13 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    crate::front_of_house::hosting::add_to_waitlist;
+
+    // relative path
+    front_of_house::hosting::add_to_waitlist;
+}
