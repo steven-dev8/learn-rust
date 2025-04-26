26d50330c2a739c9a709bb07bc36d1205f4c2aca
stevenn-dev8 <stevenndev8@gmail.com>
2025-04-26 16:20:44 -0300

    cap9 unrecoverable errors

---
+++ cap9_1_unrecoverable/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // panic!("crash and burn");
+    // panic para o programa, usado principalmente para 
+    // erros não recuperáveis
+    
+
+    // Erro irrecuperável
+    let v = vec![1, 2, 3];
+    v[99];
+
+
+}
